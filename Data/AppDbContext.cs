62064f51b3e93ce39c8b3a427e5ee0af0a9e6239
Dastan <dasistar02@gmail.com>
2026-05-28 17:18:50 +0600

    crud

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using CrudApi.Models;
+
+namespace CrudApi.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Products => Set<Product>();
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+using CrudApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+var app = builder.Build();
+
+app.MapControllers();
+app.Run();
